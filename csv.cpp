e4f8e8593046d538bb96e11a2753bd47ae010176
youralien <ryan.louie@students.olin.edu>
2015-11-27 22:35:05 -0500

    add csv, randomhash, notes on compiling to an executable

---
+++ csv.cpp
@@ -0,0 +1,13 @@
+#include <fstream>      // std::ofstream
+
+int main () {
+
+  std::ofstream ofs;
+  ofs.open ("test.txt", std::ofstream::out | std::ofstream::app);
+
+  ofs << " more lorem ipsum";
+
+  ofs.close();
+
+  return 0;
+}
